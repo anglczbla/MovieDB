9b5ee7e4524a5a7d3d9384b416046cddad153a05
angel <angelicaizabella90@gmail.com>
2025-07-03 16:15:29 +0700

    hit new api

--- src/services/api.js
+++ src/services/api.js
@@ -5,7 +5,7 @@ export const movieAPI = {
   // Ambil film populer
   getPopularMovies: async (page = 1) => {
     const response = await fetch(
-      `${BASE_URL}/movie/popular?api_key=${API_KEY}&page=${page}`
+      `${BASE_URL}/movie/popular?api_key=${API_KEY}&language=en-US&page=${page}`
     );
     return response.json();
   },
@@ -13,20 +13,21 @@ export const movieAPI = {
   // Search film
   searchMovies: async (query, page = 1) => {
     const response = await fetch(
-      `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${query}&page=${page}`
+      `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${query}&language=en-US&page=${page}`
     );
     return response.json();
   },
 
   // Detail film
   getMovieDetail: async (id) => {
-    const response = await fetch(`${BASE_URL}/movie/${id}?api_key=${API_KEY}`);
+    const response = await fetch(`${BASE_URL}/movie/${id}?api_key=${API_KEY}&language=en-US`);
     return response.json();
   },
 
+  // Discover film berdasarkan genre
   discoverByGenre: async (genreId) => {
     const response = await fetch(
-      `${BASE_URL}/discover/movie?api_key=${API_KEY}&with_genres=${genreId}`
+      `${BASE_URL}/discover/movie?api_key=${API_KEY}&with_genres=${genreId}&language=en-US`
     );
     return response.json();
   },
@@ -34,7 +35,63 @@ export const movieAPI = {
   // Ambil genre
   getGenres: async () => {
     const response = await fetch(
-      `${BASE_URL}/genre/movie/list?api_key=${API_KEY}`
+      `${BASE_URL}/genre/movie/list?api_key=${API_KEY}&language=en-US`
+    );
+    return response.json();
+  },
+
+  // Ambil orang populer
+  getPopularPeople: async (page = 1) => {
+    const response = await fetch(
+      `${BASE_URL}/person/popular?api_key=${API_KEY}&language=en-US&page=${page}`
+    );
+    return response.json();
+  },
+
+  // Ambil tren semua (movie, TV, dll) harian
+  getTrendingAllDay: async () => {
+    const response = await fetch(
+      `${BASE_URL}/trending/all/day?api_key=${API_KEY}&language=en-US`
+    );
+    return response.json();
+  },
+
+  // Ambil tren film harian
+  getTrendingMoviesDay: async () => {
+    const response = await fetch(
+      `${BASE_URL}/trending/movie/day?api_key=${API_KEY}&language=en-US`
+    );
+    return response.json();
+  },
+
+  // Ambil tren orang harian
+  getTrendingPeopleDay: async () => {
+    const response = await fetch(
+      `${BASE_URL}/trending/person/day?api_key=${API_KEY}&language=en-US`
+    );
+    return response.json();
+  },
+
+  // Ambil tanggal rilis film
+  getMovieReleaseDates: async (movieId) => {
+    const response = await fetch(
+      `${BASE_URL}/movie/${movieId}/release_dates?api_key=${API_KEY}&language=en-US`
+    );
+    return response.json();
+  },
+
+  // Ambil film top rated
+  getTopRatedMovies: async (page = 1) => {
+    const response = await fetch(
+      `${BASE_URL}/movie/top_rated?api_key=${API_KEY}&language=en-US&page=${page}`
+    );
+    return response.json();
+  },
+
+  // Ambil film yang akan datang
+  getUpcomingMovies: async (page = 1) => {
+    const response = await fetch(
+      `${BASE_URL}/movie/upcoming?api_key=${API_KEY}&language=en-US&page=${page}`
     );
     return response.json();
   },
